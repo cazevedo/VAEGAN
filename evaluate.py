33c669e4410467da0da8992b0b64af7377dd6e38
Carlos Azevedo <cguerraazevedo@tecnico.ulisboa.pt>
2018-12-17 14:58:15 +0000

    added configuration file; added skeleton of evaluate module

---
+++ evaluate.py
@@ -0,0 +1,11 @@
+import missing_generator
+import reconstruct
+import performance_eval
+
+def main():
+    missing_generator.run()
+    reconstruct.run()
+    performance_eval.run()
+
+if __name__ == "__main__":
+    main()
