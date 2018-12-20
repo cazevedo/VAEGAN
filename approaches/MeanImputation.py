fa42cf0e92834e2e542ac0d816105ac33c8245df
cazevedo <cmaz90@gmail.com>
2018-12-20 19:30:53 +0000

    added Mean Imputation and Most Frequent baseline methods;

---
+++ approaches/MeanImputation.py
@@ -0,0 +1,39 @@
+import numpy as np
+import pandas as pd
+
+def reconstruct(dataset, mask):
+    (datasetLen, dim) = np.shape(dataset)
+
+    incomplete_dataset = pd.DataFrame(dataset.copy())
+    reconstructed_dataset = pd.DataFrame(np.zeros((datasetLen, dim)))
+
+    for i in range(datasetLen):
+        frame = incomplete_dataset.loc[i, :]
+        mean = frame.mean()
+        ms = mask.loc[i, :]
+        frame.values[ms.values == 0] = mean
+        reconstructed_dataset.loc[i, :] = frame.values
+
+    return reconstructed_dataset
+
+## DEBUG TOOLS ##
+# import matplotlib.pyplot as plt
+# from tqdm import tqdm
+# import reconstruct as rc
+# if __name__ == "__main__":
+#     original_dataset, incomplete_dataset, mask = rc.get_dataset(mode='MNAR', n_samples=20)
+#
+#     original_dataset = pd.DataFrame(original_dataset)
+#     incomplete_dataset = pd.DataFrame(incomplete_dataset)
+#     mask = pd.DataFrame(mask)
+#
+#     reconstructed_dataset = reconstruct2(incomplete_dataset, mask)
+#
+#     inc = incomplete_dataset.loc[0, :]
+#     rec = reconstructed_dataset.loc[0,:]
+#     orig = original_dataset.loc[0,:]
+#
+#     samples = np.vstack([inc, rec, orig])
+#     fig = rc.plot(samples)
+#     plt.savefig('Multiple_Impute_out1/{}.png'.format(str(0).zfill(3)), bbox_inches='tight')
+#     plt.close(fig)
